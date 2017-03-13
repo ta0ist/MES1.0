ebdce12922afc8f6e778fa8bb2b40b0305a9769e
xjccloud <248655942@qq.com>
2017-03-13 14:30:55 +0800

    commit

--- Module/Common/node/controller/Main.js
+++ Module/Common/node/controller/Main.js
@@ -2,6 +2,6 @@ var path = require('path');
 var session = require('express-session');
 exports.Index = function(req, res) {
     // if (!req.session.user)
-    res.redirect('/login');
-    //res.render(res.render(path.resolve(__dirname, '../../web/view/main/index')));
+    res.redirect('/');
+    res.render(res.render(path.resolve(__dirname, '../../web/view/main/index')));
 }
