9dc74025d27b8f544dd7800cf43d19ff9a03647a
ZSY931015 <510856339@qq.com>
2017-03-15 16:56:55 +0800

    commit

--- Module/Common/web/controller/userController.js
+++ Module/Common/web/controller/userController.js
@@ -1,3 +1,19 @@
-  angular.module('user', []).controller('userCtrl', function($scope, $http) {
+  $(document).ready(function() {
+
+      $('label.tree-toggler').click(function() {
+          var icon = $(this).children(".fa");
+          if (icon.hasClass("fa-folder-o")) {
+              icon.removeClass("fa-folder-o").addClass("fa-folder-open-o");
+          } else {
+              icon.removeClass("fa-folder-open-o").addClass("fa-folder-o");
+          }
+
+          $(this).parent().children('ul.tree').toggle(300, function() {
+              $(this).parent().toggleClass("open");
+              $(".tree .nscroller").nanoScroller({ preventPageScrolling: true });
+          });
+
+      });
+
 
   })
